70a2f96708b96b9c4f95b23febd13150e85434e3
Oddbjørn Borge-Jensen <oddbjorn.sb@gmail.com>
2022-08-23 15:00:49 +0200

    CHORE: Changed header links to image and user name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,30 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "../style//Header.css"
 
-function Header(props) {
+function Header() {
+    const user = useSelector(state => state.user);
+    const navigate = useNavigate();
 
-    const { user } = props;
+    function logoOnClick() {
+        if(user.username !== "") {
+            navigate("translate");
+        }
+    }
+
+    function profileOnClick() {
+        if(user.username !== "") {
+            navigate("profile");
+        }
+    }
 
     return (
         <div className="header">
-                <Link to="translate">Home</Link>
-                <h4>{ user }</h4>
-                <Link to="profile">Profile</Link>
+            <div onClick={logoOnClick} className="title">
+                <img className="header-image" src="Logo.png" alt="Logo"></img>
+                <h3 className="title-text">Lost in Translation</h3>
+            </div>
+            <h2 onClick={profileOnClick} className="name">{ user.username }</h2>
         </div>
     );
 }
